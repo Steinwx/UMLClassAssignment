d8c9bfbac586fa5b2a7cef803f39986a5045d888
Steinwx <134057857+Steinwx@users.noreply.github.com>
2023-05-19 20:08:39 +0800

    Update ClassTV.py

--- ClassTV.py
+++ ClassTV.py
@@ -3,3 +3,14 @@ class TV:
         self.channel = 1
         self.volumeLevel = 1
         self.on = False
+    def turnOn(self):
+    def turnOff(self):
+    def getChannel(self):
+    def setChannel(self):
+    def getVolume(self):
+    def setVolume(self):   
+    def channelUp(self):
+    def channelDown(self):
+    def volumeUp(self):
+    def volumeDown(self):
+         
