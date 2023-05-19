f6dd716015d7d0bfdc768e18fff4392329ad60cb
Steinwx <134057857+Steinwx@users.noreply.github.com>
2023-05-19 20:41:45 +0800

    Update ClassTV.py

--- ClassTV.py
+++ ClassTV.py
@@ -1,16 +1,43 @@
 class TV:
+    
     def __init__(self) -> None:
         self.channel = 1
         self.volumeLevel = 1
         self.on = False
+
     def turnOn(self):
+        self.on= True
+
     def turnOff(self):
+        self.on= False
+
     def getChannel(self):
-    def setChannel(self):
-    def getVolume(self):
-    def setVolume(self):   
+        return self.channel
+    
+    def setChannel(self,int):
+        if int>=120 and int<=1:
+            self.channel=int
+
     def channelUp(self):
+        if self.channel<120:
+            self.channel+=1
+        else:
+            self.channel=1
+
     def channelDown(self):
-    def volumeUp(self):
+        if self.channel>1:
+            self.channel-=1
+        else:
+            self.channel=120
+
+    def volumelUp(self):
+        if self.channel<7:
+            self.channel+=1
+        else:
+            self.channel=1
+
     def volumeDown(self):
-         
+        if self.channel>1:
+            self.channel-=1
+        else:
+            self.channel=7
